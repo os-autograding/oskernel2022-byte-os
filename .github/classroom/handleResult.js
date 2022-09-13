e584d8bd309f4439555344ba16c2364832fc9beb
yufeng <321353225@qq.com>
2022-09-13 16:26:12 +0800

    测试github

--- .github/classroom/handleResult.js
+++ .github/classroom/handleResult.js
@@ -1,5 +1,8 @@
 function run({points, availablePoints}, { log, github, request }) {
     console.log(github);
+    console.log(github.sender);
+    console.log(github.pusher);
+    console.log(github.repository)
     log(request.post);
 }
 
