fe4b617742afa04a1ef3c6d44cfcdd9195ec07ae
yufeng <321353225@qq.com>
2022-09-13 16:04:42 +0800

    测试request

--- .github/classroom/handleResult.js
+++ .github/classroom/handleResult.js
@@ -1,9 +1,9 @@
 function run(points, availablePoints, log) {
-    let github = require("@actions/github")
+    // let github = require("@actions/github")
     let request = require("request");
     console.log(request);
-    console.log(github);
-    log("github actor: ", github.actor)
+    // console.log(github);
+    // log("github actor: ", github.actor)
 }
 
 module.exports.run = run;
